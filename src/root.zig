8929beab31f67bf307d8f2f197689bcbe681f309
JM <jacobmontpetit@gmail.com>
2025-03-23 23:37:29 -0400

    root: add dispatch loop setfilter compile

--- src/root.zig
+++ src/root.zig
@@ -10,6 +10,8 @@ const PCAP_C = @cImport({
 
 pub const pcap = PCAP_C.pcap_t;
 pub const pktHeader = PCAP_C.pcap_pkthdr;
+pub const bfp_program = PCAP_C.bpf_program;
+pub const pcapHandler = PCAP_C.pcap_handler;
 
 pub fn create(pcapFile: [:0]const u8, errorBuffer: []u8) ?*pcap {
     return PCAP_C.pcap_create(pcapFile, errorBuffer.ptr);
@@ -19,12 +21,28 @@ pub fn activate(p: ?*pcap) c_int {
     return PCAP_C.pcap_activate(p);
 }
 
+pub fn compile(p: ?*pcap, fp: ?*bfp_program, str: [:0]const u8, optimize: c_int, netmask: c_uint) isize {
+    return PCAP_C.pcap_compile(p, fp, str, optimize, netmask);
+}
+
+pub fn setfilter(p: ?*pcap, fp: ?*bfp_program) isize {
+    return PCAP_C.pcap_setfilter(p, fp);
+}
 pub fn next_ex(p: ?*pcap, hdr: *?*pktHeader, data: *?*const u8) isize {
     return PCAP_C.pcap_next_ex(p, @alignCast(hdr), @alignCast(data));
 }
+pub fn loop(p: ?*pcap, cnt: c_int, hdlr: ?*pcapHandler, user: [:0]u8) isize {
+    return PCAP_C.pcap_loop(p, cnt, hdlr, user);
+}
+
+pub fn dispatch(p: ?*pcap, cnt: c_int, hdlr: pcapHandler, user: [:0]u8) isize {
+    return PCAP_C.pcap_dispatch(p, cnt, hdlr, user);
+}
+
 pub fn sendpacket(p: ?*pcap, buf: []const u8) isize {
     return PCAP_C.pcap_sendpacket(p, buf.ptr, @intCast(buf.len));
 }
+//
 //pcap_open_live
 pub fn open_live(
     device: [:0]const u8, //
