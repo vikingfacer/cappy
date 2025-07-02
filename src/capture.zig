916175bdbcecd52574ae820f125cf0d1ac6ae33e
JM <jacobmontpetit@gmail.com>
2025-07-01 22:27:08 -0400

    capture: print capture as hex

--- src/capture.zig
+++ src/capture.zig
@@ -10,9 +10,17 @@ pub fn live(out: anytype, device: [:0]const u8, filter: [:0]const u8) !void {
             compiled_filter = f;
             _ = liveCapture.setfilter(&compiled_filter);
             var header: ?*pcap.pktHeader = null;
-            var data: ?*const u8 = null;
+            var data: ?[*]const u8 = null;
             while (liveCapture.next_ex(&header, &data) >= 0) {
-                try out.print("{*}\n", .{data});
+                for (0..header.?.caplen) |i| {
+                    if (i % 16 == 0) {
+                        try out.print("\n{x:0>8} ", .{i});
+                    }
+                    try out.print("{x:02}", .{data.?[i]});
+                    if ((i + 1) % 2 == 0) {
+                        try out.print(" ", .{});
+                    }
+                }
             }
         }
     } else {
