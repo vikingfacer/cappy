1dd5e8d712d7914c79d42fe420f87bbdc2a9d3df
JM <jacobmontpetit@gmail.com>
2025-11-16 21:10:58 -0500

    main clasp & 15.1 and print errors w/ debug

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,13 @@ const listdev = @import("listDevices.zig");
 const runner = @import("capture.zig");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    // First we specify what parameters our program can take.
-    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
-    const params = comptime clap.parseParamsComptime(
+    const cappyArgs =
         \\-h, --help            Display this help and exit.
         \\-l, --list            List devices to listen upon
         \\-p, --program <STR>   Dynamically loaded program
@@ -19,10 +19,9 @@ pub fn main() !void {
         \\-i, --input <STR>     Pcap File
         \\-o, --output <STR>    output Pcap File
         \\<STR>...
-    );
+    ;
+    const params = comptime clap.parseParamsComptime(cappyArgs);
 
-    // Declare our own parsers which are used to map the argument strings to other
-    // types.
     const parsers = comptime .{
         .STR = clap.parsers.string,
     };
@@ -35,54 +34,48 @@ pub fn main() !void {
         // allowed.
         .assignment_separators = "=:",
     }) catch |err| {
-        diag.report(std.io.getStdErr().writer(), err) catch {};
+        try diag.reportToFile(.stderr(), err);
         return err;
     };
     defer res.deinit();
 
-    if (res.args.help != 0)
-        std.debug.print("--help\n", .{});
-    if (res.args.list != 0) {
+    if (res.args.help != 0) {
+        std.debug.print("{s}\n", .{cappyArgs});
+    } else if (res.args.list != 0) {
         try listdev.listdevices(stdout);
-    }
+    } else {
+        const device: []const u8 = res.args.device orelse "any";
 
-    const device: []const u8 = res.args.device orelse "any";
-
-    var errorBuffer = [_]u8{0} ** 2048;
-    var capture: pcap.pcapture = undefined;
-    if (res.args.input) |file| {
-        if (pcap.create(file, &errorBuffer)) |fileOpen| {
-            capture = fileOpen;
+        var errorBuffer = [_]u8{0} ** 2048;
+        var capture: pcap.pcapture = undefined;
+        if (res.args.input) |file| {
+            if (pcap.open_offline(file, &errorBuffer)) |fileOpen| {
+                capture = fileOpen;
+            } else {
+                std.debug.print("Unable to open file: {s}\n", .{file});
+                std.debug.print("{s}\n", .{errorBuffer});
+            }
         } else {
-            try stdout.print("Unable to open file: {s}\n", .{file});
-            try stdout.print("{s}\n", .{errorBuffer});
+            if (pcap.open_live(device, 4096, 1, 1000, &errorBuffer)) |liveOpen| {
+                capture = liveOpen;
+            } else {
+                std.debug.print("Unable to open device program requires root access\n", .{});
+                std.debug.print("{s}\n", .{errorBuffer});
+            }
         }
-    } else {
-        if (pcap.open_live(device, 4096, 1, 1000, &errorBuffer)) |liveOpen| {
-            capture = liveOpen;
-        } else {
-            try stdout.print("Unable to open device program requires root access\n", .{});
-            try stdout.print("{s}\n", .{errorBuffer});
+        defer capture.close();
+
+        const filter = std.mem.joinZ(gpa.allocator(), " ", res.positionals[0]) catch "";
+        defer gpa.allocator().free(filter);
+        if (filter.len > 0) {
+            if (capture.compile(filter, 0, 0)) |f| {
+                var nonConstf = f;
+                _ = capture.setfilter(&nonConstf);
+            } else {
+                std.debug.print("Unable to set filter {s}", .{filter});
+            }
         }
-    }
-    defer capture.close();
 
-    const filter = std.mem.joinZ(gpa.allocator(), " ", res.positionals[0]) catch "";
-    defer gpa.allocator().free(filter);
-    if (capture.compile(filter, 0, 0)) |f| {
-        var nonConstf = f;
-        _ = capture.setfilter(&nonConstf);
-    } else {
-        try stdout.print("Unable to set filter {s}", .{filter});
-    }
-
-    if (res.args.program) |program| {
-        var seq = std.mem.splitSequence(u8, program, ":");
-        try stdout.print("{?s} {?s}", .{ seq.first(), seq.next() });
-        const lib = seq.first();
-        const fun = seq.next().?;
-        try runner.dispatcher(capture, lib, fun);
-    } else {
         try runner.live(stdout, capture);
     }
 }
